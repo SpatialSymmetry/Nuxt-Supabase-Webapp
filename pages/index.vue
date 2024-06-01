e91b3af687acd88f92792492722432cc26a666a5
spatialsymmetry <spatialsymmetry@gmail.com>
2024-06-02 02:05:40 +0330

    fix responsive

--- pages/index.vue
+++ pages/index.vue
@@ -22,11 +22,13 @@
 
 
 <section v-if="googleScholar" class="w-full min-h-screen bg-gray-200 py-10 ">
-    <form @submit.prevent="handleSearch" class="lg:w-3/4 max-w-screen-md mx-auto flex gap-2">
+  <div class="lg:w-3/4 max-w-screen-md mx-auto ">
+    <form @submit.prevent="handleSearch" class="mx-5 flex gap-2">
       <input type="text" v-model="query" placeholder="Search will be directed to scholar.google.com"
       class="w-full border border-gray-300 rounded-md p-2 pl-4 focus:outline-none">
       <button @click="submit" class="bg-blue-500 p-2 hover:bg-blue-700 text-white font-bold rounded hover:cursor-pointer">Search</button>
     </form>
+  </div>
 </section>
 
 
