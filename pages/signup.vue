d3afefa78fb8e520399754711450d99f9db3f8c3
spatialsymmetry <spatialsymmetry@gmail.com>
2024-06-20 20:06:07 +0330

    email fixed

--- pages/signup.vue
+++ pages/signup.vue
@@ -1,52 +1,38 @@
 
 <template>
 
-<!-- Hero Section -->
 
-  <div class="flex items-center justify-center min-h-screen bg-gray-200 px-4">
+  <div class="min-h-screen bg-gray-200 px-4 flex items-center justify-center">
 
-    <div class="w-full max-w-md p-8 space-y-6 bg-white rounded-lg shadow-lg">
+    <div class="max-w-md p-8 bg-white rounded-lg shadow-lg">
 
-      <h1 class="text-center text-xl tracking-tight">ECO-SET Litrature Review Portal Sign Up.</h1>
+      <h1 class="mb-6 text-center text-xl tracking-tight">ECO-SET Litrature Review Portal Sign Up.</h1>
 
-      <form @submit.prevent="signup" class="space-y-4">
+      <form @submit.prevent="signup" class="flex flex-col gap-4">
 
-        <div class="pb-2">
-          <label for="email" class="pb-3 block text-sm font-medium text-gray-700">Enter Your Email:</label>
-          <input type="email" v-model="email" required 
-            class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400
-            focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-          <p v-if="emailError" class="text-red-500 text-sm mt-2">{{ emailError }}</p>
+        <div class="flex flex-col gap-3">
+          <label for="email" class="text-sm font-medium text-gray-700">Enter Your Email:</label>
+          <input type="email" v-model="form.email" required 
+            class="px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400
+            focus:outline-none focus:border-indigo-500 sm:text-sm">
+          <!-- <p v-if="emailError" class="text-red-500 text-sm">{{ emailError }}</p> -->
         </div>
 
-        <div>
-          <label for="password" class="block text-sm font-medium text-gray-700">Set Your Account's Password: (It has to contain atleast 8 character, upper and lower case letters, a number and a symbol.)</label>
-          <input type="password" v-model="password" required 
-            class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+        <div class="flex flex-col gap-3">
+          <label class="text-sm font-medium text-gray-700">Set Your Account's Password: (It has to contain atleast 8 character, upper and lower case letters, a number and a symbol.)</label>
+          <input type="password" v-model="form.password" required 
+            class="px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:border-indigo-500 sm:text-sm">
+        </div>
 
+        <div class="flex flex-col gap-3">
           <p class="mt-3 block text-sm font-medium text-gray-700">Verify Your Password</p>
-          <input type="password" v-model="password_confirm" required 
-            class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-
-          <p class="mt-3 block text-sm font-medium text-gray-700">Enter Your First Name:</p>
-          <input type="password" v-model="password_confirm" required 
-            class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400
-            focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-
-          <p class="mt-3 block text-sm font-medium text-gray-700">Enter Your Last Name:</p>
-          <input type="password" v-model="password_confirm" required 
-            class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400
-            focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-
-          <p class="mt-3 block text-sm font-medium text-gray-700">Select Your University/Institution:</p>
-          <input type="password" v-model="password_confirm" required 
-            class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400
-            focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-
+          <input type="password" v-model="form.password_confirm"
+            class="px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:border-indigo-500 sm:text-sm">
         </div>
 
         <div>
-          <button type="submit" :disabled="!isEmailValid"
+          <!-- <button type="submit" :disabled="!isEmailValid" -->
+          <button type="submit"
             class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm
             font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2
             focus:ring-offset-2 focus:ring-indigo-500">
@@ -67,52 +53,50 @@
 
 <script setup>
 
-definePageMeta({
-  layout: '',
-  middleware: 'auth'
-})
 
 const { $supabase } = useNuxtApp();
-onMounted(async () => {
-  const user = await $supabase.auth.getUser();
-  console.log('(signup page) User data:', user.data.user.id);
+
+const form = ref({
+  email: '',
+  password: '',
+  password_confirm: '',
 });
 
-
-const router = useRouter()
-const email = ref('')
-const allowedSuffixes = ["@gmail.com", "@organization.org", "@institution.edu"]
-const emailError = ref('')
-const password = ref('')
-const password_confirm = ref('')
-
-const isEmailValid = computed(() => {
-  // Check if the email ends with any of the allowed suffixes
-  const isValidSuffix = allowedSuffixes.some(suffix => email.value.endsWith(suffix));
-  if (isValidSuffix) {
-    emailError.value = ''
-    return true
-  } else {
-    emailError.value = `Please use a professional email that ends with one of the following: ${allowedSuffixes.join(', ')}`
-    return false
-  }
-})
-
 async function signup() {
-  if (isEmailValid.value){
     const { user, error } = await $supabase.auth.signUp({
-      email: email.value,
-      password: password.value
+      email: form.value.email,
+      password: form.value.password
     })
-    console.log("Signing up with:", email.value)
-  }
-  if (error) {
-    authError.value = error.message
-    console.log("authError:", email.value)
-  } else {
-    router.push('/confirmation')  // Redirect to a confirmation page or similar
-  }
+    console.log(form.value.email)
+    if (error){
+    console.log(error)
+    }
 }
+
+
+// definePageMeta({
+//   layout: '',
+//   middleware: 'auth'
+// })
+
+
+
+// const router = useRouter()
+// const allowedSuffixes = ["@gmail.com", "@organization.org", "@institution.edu"]
+// const emailError = ref('')
+
+
+// const isEmailValid = computed(() => {
+//   // Check if the email ends with any of the allowed suffixes
+//   const isValidSuffix = allowedSuffixes.some(suffix => form.email.value.endsWith(suffix));
+//   if (isValidSuffix) {
+//     emailError.value = ''
+//     return true
+//   } else {
+//     emailError.value = `Please use a professional email that ends with one of the following: ${allowedSuffixes.join(', ')}`
+//     return false
+//   }
+// })
 
 </script>
 
